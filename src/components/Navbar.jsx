d171336420a2c705bb542d09c0b9aa2a940be28e
vinit <vinitkumar93341@gmail.com>
2025-05-07 00:32:58 +0530

    mobile-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar() {
+  const [isOpnen, setIsOpen] = useState(false);
+  const [activeItem, setActiveitem] = useState("");
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpnen);
+  };
+
+  const handleMenuClick = (name) => {
+    setActiveitem(name);
+    setIsOpen(false);
+  };
+
   const menuItems = [
     { name: "About us", href: "#about" },
     { name: "Services", href: "#services" },
@@ -25,7 +39,7 @@ function Navbar() {
                 key={index}
                 href={item.href}
                 className="text-black px-2 hover:bg-[var(--color-primary-bg)]
-                 hover:primary-bg rounded-sm hover:underline underline-offset-4 
+                 rounded-sm hover:underline underline-offset-4 
                  transition-all duration-300"
               >
                 {item.name}
@@ -39,8 +53,49 @@ function Navbar() {
               Request a qoute
             </a>
           </div>
+
+          {/* mobile menu */}
+          <div className="md:hidden flex items-center">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-700 hover:text-[var(--color-primary-bg)] focus:outline-none"
+            >
+              {isOpnen ? (
+                <IoMdClose className="h-6 w-6" />
+              ) : (
+                <HiMenuAlt3 className="h-6 w-6" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
+
+      {/* mobile menu */}
+      {isOpnen && (
+        <div className="md:hidden bg-red shadow-md">
+          <div className="px-4 pt-2 pb-3 space-y-2 sm:px-3">
+            {menuItems.map((item, index) => (
+              <a
+                className="block text-black px-2 rounded-sm hover:underline underline-offset-4 transition-all
+                    duration-300 hover:bg-[var(--color-primary-bg)]
+                  "
+                key={index}
+                href={item.href}
+              >
+                {item.name}
+              </a>
+            ))}
+            <a
+              href="#contact"
+              className="block border px-4 py-2 border-black rounded-md transition-all duration-300
+              hover:bg-[var(--color-primary-bg)] hover:border-[var(--color-pirmary-bg)]
+              "
+            >
+              Request a qoute
+            </a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
